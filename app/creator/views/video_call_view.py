9924b0e6518d6b08e44f214c176789abd6ce7a23
vyasishanatc194 <vyasishanatc194@gmail.com>
2021-03-18 09:45:25 +0530

    Update Agora token generation API

--- app/creator/views/video_call_view.py
+++ app/creator/views/video_call_view.py
@@ -176,7 +176,39 @@ class EndCallAPIView(APIView):
             session = sessions.first()
             session.completed = True
             session.session_completed_at = datetime.now()
-            session.save()         
+            session.save()          
   
         message = "Call ended successfully!"
         return custom_response(True, status.HTTP_200_OK, message)
+
+
+    def get(self, request):
+        call_type = request.GET.get('call_type', None)
+        call_id = request.GET.get('call_id', None)
+        if not call_type or not call_id:
+            message = "call_type and call_id are required!"    
+            return custom_response(False, status.HTTP_400_BAD_REQUEST, message)
+
+        valid_call_type = ['stream', 'session']
+        if call_type not in valid_call_type:
+            message = "Invalid call_type. Choose from " + str(valid_call_type)    
+            return custom_response(False, status.HTTP_400_BAD_REQUEST, message)
+
+        if call_type == valid_call_type[0]:
+            streams = Stream.objects.filter(pk=call_id, creator=request.user.pk)
+            if not streams:
+                message = "Invalid Stream ID!"    
+                return custom_response(False, status.HTTP_400_BAD_REQUEST, message)
+            stream = streams.first()
+            completed = stream.completed
+
+        if call_type == valid_call_type[1]:
+            sessions = TimeSlot.objects.filter(pk=call_id, session__creator=request.user.pk)
+            if not sessions:
+                message = "Invalid Session ID!"    
+                return custom_response(False, status.HTTP_400_BAD_REQUEST, message)
+            session = sessions.first()
+            completed = session.completed        
+  
+        message = "Call status fetched successfully!"
+        return custom_response(True, status.HTTP_200_OK, message, {'completed': completed})
