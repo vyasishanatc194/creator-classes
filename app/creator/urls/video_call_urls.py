90ef53a1d5b5998ec4f605ecc683f3fd95f5bec5
vyasishanatc194 <vyasishanatc194@gmail.com>
2021-03-30 17:24:10 +0530

    Update token expiration time

--- app/creator/urls/video_call_urls.py
+++ app/creator/urls/video_call_urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("channel-token/", views.GenerateAgoraTokenAPIView.as_view(), name="channel-token"),
     path("session-screen-share/<int:pk>/", views.SessionScreenShareAPIView.as_view(), name="session-screen-share"),
     path("end-call/", views.EndCallAPIView.as_view(), name="end-call"),
-    path("stream-screen-share/<int:pk>/", views.SessionScreenShareAPIView.as_view(), name="stream-screen-share"),
+    path("stream-screen-share/<int:pk>/", views.StreamScreenShareAPIView.as_view(), name="stream-screen-share"),
     path("stream-users/<int:pk>/", views.StreamActiveMembersAPIView.as_view(), name="stream-users"),
 
 ]
