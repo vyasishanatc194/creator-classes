150816ece5ca1691b3000d3b2480459f5c567b18
druz2105 <druz2105@gmail.com>
2021-04-19 15:37:32 +0530

    Updeted Migrations

--- app/customadmin/models/keywords.py
+++ app/customadmin/models/keywords.py
@@ -4,7 +4,7 @@ from creator_class.models import ActivityTracking
 
 class AdminKeyword(ActivityTracking):
     keyword = models.CharField(max_length=55, blank=False, null=False, default='')
-    image = models.ImageField('keyword/')
+    image = models.ImageField(upload_to='keyword/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.keyword}"
