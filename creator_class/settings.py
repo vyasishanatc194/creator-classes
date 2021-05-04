21fa834602af77a983fea9a27d7c1dcda07a3f8d
druz2105 <druz2105@gmail.com>
2021-05-04 15:38:13 +0530

    changed affiliation link

--- creator_class/settings.py
+++ creator_class/settings.py
@@ -284,7 +284,7 @@ CURRENCY = "usd"
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880 # 5 MB
 
-USER_SIGNUP_LINK = "http://ccmike.creatorclasses.co/"
+USER_SIGNUP_LINK = "http://ccmike.creatorclasses.co/user/signup/"
 RESET_PASSWORD_LINK = "http://3.139.122.63/reset-password/"
 
 
