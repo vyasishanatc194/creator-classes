c8ce9a67d3d6ced64753c2c6fe97f0540eee6daf
druz2105 <druz2105@gmail.com>
2021-06-07 14:56:09 +0530

    Updae in path of customadmin

--- app/customadmin/views/streams.py
+++ app/customadmin/views/streams.py
@@ -87,7 +87,7 @@ class StreamCreateView(MyNewFormsetCreateView):
         
         context = super().get_context_data( *args, **kwargs)
         context.update(get_aws_s3_creds())
-        context['path'] = 'public/stream'
+        context['path'] = 'public/streams'
         return context
 
 
@@ -122,7 +122,7 @@ class StreamUpdateView(MyNewFormsetUpdateView):
         
         context = super().get_context_data( *args, **kwargs)
         context.update(get_aws_s3_creds())
-        context['path'] = 'public/stream'
+        context['path'] = 'public/streams'
         return context    
 
     def get_success_url(self):
